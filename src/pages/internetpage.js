e0e9d046be031adca5f726a523c6cb7d4bbe2b2a
Leonardo Sánchez <lordcreos@gmail.com>
2022-07-25 23:37:45 -0500

    creating desktop view

--- src/pages/internetpage.js
+++ src/pages/internetpage.js
@@ -22,7 +22,8 @@ function InternetView() {
               </div>
             </div>
 
-      <section className='flex p-4 '>
+  <div className=' sm:flex sm:justify-center sm:mt-6'>
+        <section className='flex p-4 sm:bg-white sm:rounded-l-md '>
         <RouterIcon/>
         <div className='flex-col p-5'>
         <div className='text-tiny text-[#707070]'>Service Status</div> 
@@ -32,22 +33,28 @@ function InternetView() {
         </div>
       </section>
 
-      <section className='flex p-4 space-x-4 '>
-      <div className='flex-1' >
+      <section className='flex p-4 space-x-4 sm:p-0 sm:space-x-4'>
+        
+      <div className='flex-1 sm:bg-white sm:rounded-r-md sm:p-4 '  >
           <div className='text-tiny text-[#707070]'>Internet Plan</div> 
           <div className='inline-block px-1 mt-1 font-bold text-white rounded-sm bg-primary text-tiny'>
           Internet Service
            </div>
            <div className='font-medium text-black text-tiny '>Includes 40mb/s</div> 
       </div>
+     
       <hr/>
-      <div className='flex-1' >
+      <div className='flex-1 sm:bg-white sm:rounded-md sm:p-4' >
         <div className='text-tiny text-[#707070]'>Equipment Information</div> 
         <div className='font-medium text-black text-tiny'>Digital Max Pro/100 mbps </div> 
       </div>
       </section>
 
-      <section className='flex px-4 py-1 place-content-between'>
+    </div>
+
+
+   
+      <section className='flex px-4 py-1 place-content-between sm:mx-auto sm:w-[50%] sm:mt-10'>
       <div className='text-xl font-bold text-primary' >
       Additional services
       </div>
@@ -56,11 +63,23 @@ function InternetView() {
       + Add more services
       </div>
       </section>
-      <div className='py-4 text-center text-tiny'> You have the following services included with your plan</div>
-      <section className='p-4 '>
-        <div className='bg-white rounded-md shadow-xl '> 
-        <div className='flex p-5 place-content-between'>
+      <div className='p-4 text-center sm:mx-auto sm:w-[50%] sm:text-left text-tiny'> You have the following services included with your plan</div>
 
+      
+      <section className='p-4 sm:mx-auto sm:w-[65%] sm:flex sm:space-x-4 '>
+
+        <div className='my-2 bg-white rounded-md shadow-xl sm:w-2/4 '> 
+          <div className='flex p-5 place-content-between'>
+            <div> 
+              <div><span className='text-xl font-bold text-primary '> Basic</span> <span className='text-[#707070] text-xl font-bold '> 40mb/s</span></div>
+              <div className='text-tiny'> Virus protection and more</div>
+            </div>
+            <div> <IoTrashOutline className='text-primary'/></div>
+         </div>
+        </div>
+        
+        <div className='my-2 bg-white rounded-md shadow-xl sm:w-2/4'> 
+          <div className='flex p-5 place-content-between'>
             <div> 
               <div><span className='text-xl font-bold text-primary '> Basic</span> <span className='text-[#707070] text-xl font-bold '> 40mb/s</span></div>
               <div className='text-tiny'> Virus protection and more</div>
@@ -70,14 +89,15 @@ function InternetView() {
         </div>
         
       </section>
-      
-      <section className='flex px-4 py-1 place-content-between'>
+
+      <div className='sm:mx-auto sm:w-[65%]'>
+       <section className='flex px-4 py-1 place-content-between'>
             <div className='text-xl font-bold text-primary' >
                Special for You
             </div>
       </section>
 
-      <section className='p-4 '>
+      <section className='flex p-4'>
         <div className='  rounded-md bg-gradient-to-b from-[#00E5D9] to-[#333366]'> 
         <div className='flex p-5 place-content-between'>
 
@@ -91,9 +111,16 @@ function InternetView() {
             </div>
          </div>
         </div>
+        <div className=' bg-[#FF8C3E] text-center -mt-[50px] text-white text-lg font-bold rounded-full h-[71px] w-[71px] p-3'>
+            20% off
+            </div>
         
       </section>
 
+
+      </div>
+      
+     
       </>
 
       
